8777eb7858a652c3f24a726eab1c12f8bed3bbcb
Dina Bakry <dinabakry47@gmail.com>
2021-12-17 23:31:27 +0200

    adding new sections

--- js/main.js
+++ js/main.js
@@ -3,18 +3,31 @@
 let scrollDown = document.querySelector("header .scroll-down");
 scrollDown.onclick = function () {
     window.scrollTo({
-        top: (window.innerHeight * 50 ) / 100,
+        top: (window.innerHeight * 50) / 100,
         behavior: "smooth"
     })
 }
 
 // toggle dropdown menu
-let dropdownButton =  document.querySelectorAll(".dropdown .dropdown-button");
-let dropdownMenu =  document.querySelectorAll(".dropdown .dropdown-menu");
+let dropdownButton = document.querySelectorAll(".dropdown .dropdown-button");
+let dropdownMenu = document.querySelectorAll(".dropdown .dropdown-menu");
 
-for(let i = 0; i < dropdownButton.length; i++){
+for (let i = 0; i < dropdownButton.length; i++) {
     dropdownButton[i].onclick = function (e) {
         e.preventDefault();
         this.parentElement.querySelector(".dropdown-menu").classList.toggle("active");
     }
 }
+//counter section
+$(function () {
+    $('.counter').counterup(
+        {
+            delay: 20,
+            time: 2000,
+
+        }
+    )
+}
+
+
+)
