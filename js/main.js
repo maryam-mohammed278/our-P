482af7b9b337ab47cdf5128a1e8ba71b2feda5d2
ahmed <ahmed5000hazem@gmail.com>
2021-12-23 23:48:55 +0200

    after adding the features section, editing the header and fixing some bugs

--- js/main.js
+++ js/main.js
@@ -19,3 +19,20 @@ for (let i = 0; i < dropdownButton.length; i++) {
     }
 }
 
+// Image Preview
+let imgPreview = document.querySelector(".image-preview")
+let imgsOfGallery = document.querySelectorAll(".gallery img")
+imgsOfGallery.forEach(img => {
+    img.onclick = function () {
+        let src = this.getAttribute("src");
+        imgPreview.querySelector("img").setAttribute("src", src);
+        imgPreview.style.zIndex = "3"
+        imgPreview.style.opacity = "1"
+        document.body.style.overflow = "hidden"
+    }
+});
+imgPreview.querySelector("button").onclick = function () {
+    imgPreview.style.opacity = "0"
+    imgPreview.style.zIndex = "-3"
+    document.body.style.overflow = "auto"
+}
