8d733edf0615097688db40a0c631b57ce1d9ffd5
maryam <maryam-mohammed278@gmail.com>
2021-12-26 15:48:12 +0200

    starting new page -> department

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 
 // scroll to bottom btn
 let scrollDown = document.querySelector("header .scroll-down");
-scrollDown.onclick = function () {
-    window.scrollTo({
-        top: (window.innerHeight * 50) / 100,
-        behavior: "smooth"
-    })
+if (scrollDown){
+    scrollDown.onclick = function () {
+        window.scrollTo({
+            top: (window.innerHeight * 50) / 100,
+            behavior: "smooth"
+        })
+    }
 }
 
 // toggle dropdown menu
